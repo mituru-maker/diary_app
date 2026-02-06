373f72310d6c021748af2a65fb60a77003e726ef
mituru-maker <mituru555uda@gmail.com>
2026-02-06 20:00:03 +0900

    Fix JavaScript errors with HTML renderer

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "7b33f6a81ba8d09b8e9507c84ecdabb2",
+"flutter_bootstrap.js": "ebe1c1a74a6e1387218df7f3e8d48fb5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "546db3585f2ea29d40c92b2a9733fbe3",
 "/": "546db3585f2ea29d40c92b2a9733fbe3",
-"main.dart.js": "501bb5b1d36f20ba22fe2cd0f1dbf720",
+"main.dart.js": "9454029a2857ea3708e50ca014bad957",
 "manifest.json": "283ebfc5b541f0d22ac06d713d9fc82d",
 "version.json": "0ccfa6a71c0ab58fcbdd5c1237ed709f"};
 // The application shell files that are downloaded before a service worker can
